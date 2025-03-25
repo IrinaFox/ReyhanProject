033440d208b5cc2c2caa08aa5edc57c93968861c
Reyhan <reyhan.salimova.rs@gmail.com>
2025-03-26 02:28:44 +0400

    test-2:added test utils.js:function maxNumber

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
 console.log("hi")
 console.log("test")
+function maxNumber(num){
+    maxNumber = num[0]
+    num.forEach((index)=>{
+        if(index>maxNumber){
+            maxNumber=index
+        }
+        return maxNumber
+    })
+}
