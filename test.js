4cdf73314ad01f3115004276848f17185cfa9644
Reyhan <reyhan.salimova.rs@gmail.com>
2025-04-03 14:15:20 +0400

    test-2:added test utils.js:function maxNumber,function minNumber

--- test.js
+++ test.js
@@ -1,35 +1,38 @@
 console.log("hi")
 console.log("test")
-function getMaxNumber(nums){
-    let maxNumber = nums[0]
+
+function getMaxNumber(nums) {
+    let maxNumber = nums[0];
     nums.forEach((value)=>{
-        if(value>maxNumber){
-            maxNumber=value
+
+        if (value > maxNumber) {
+            maxNumber = value;
         }
     })
-    return maxNumber
+    return maxNumber;
 }
 
 function getMinNumber(nums){
-    let minNumber = nums[0]
+    let minNumber = nums[0];
     nums.forEach((value)=>{
-        if(value<minNumber){
-            minNumber=value
+        if(value < minNumber){
+            minNumber = value;
         }
     })
     return minNumber
 }
 
 function getSumNum(num) {
-    let sum = 0
+    let sum = 0;
     num.forEach(num => sum += num);
-    return sum
+    return sum;
 }
 
     //test
-    const result=getMaxNumber([1,2,5,7,8,3]);
+    let numbers = [1,2,5,7,8,3,];
+    const result=getMaxNumber(numbers);
     console.log (result);
-    const result1=getMinNumber([1,2,5,7,8,3]);
+    const result1=getMinNumber(numbers);
     console.log(result1);
-    const result2=getSumNum([1,2,5,7,8,3]);
+    const result2=getSumNum(numbers);
     console.log(result2);
