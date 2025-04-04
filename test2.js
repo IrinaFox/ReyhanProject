558fc86f738c0173f54c486767544a9ecf8c79db
Reyhan <reyhan.salimova.rs@gmail.com>
2025-04-04 14:30:59 +0400

    test2.js: function countNumbers, function sayHi, function getFullName

---
+++ test2.js
@@ -0,0 +1,15 @@
+function countNumbers(a, b) {
+    return (a + b);
+}
+
+function sayHi(name) {
+    return ('Hi ' + name + '!');
+}
+
+function getFullName(firstName, lastName) {
+    return (firstName + ' ' + lastName);
+}
+   //test
+console.log(countNumbers(2, 5));
+console.log(sayHi("Ira"));
+console.log(getFullName("Ira","Ruban"))
