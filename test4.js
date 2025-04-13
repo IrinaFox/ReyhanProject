5b721723f27f9b2012d4c07fd9fc35351989fd1f
Reyhan <reyhan.salimova.rs@gmail.com>
2025-04-14 03:08:33 +0400

    DP-24:Create new test4.js file with functions doubleNumbers,toUpperCaseArray

--- test4.js
+++ test4.js
@@ -4,7 +4,13 @@ function doubleNumbers(arr) {
 }
 
 
+function toUpperCaseArray(arr) {
+    arr = arr.map((num) => num.toUpperCase());
+    return arr;
 
+}
 
 //test
 console.log(doubleNumbers([1,2,3]));
+animals = ['rizik','banan'];
+console.log(toUpperCaseArray(animals));
