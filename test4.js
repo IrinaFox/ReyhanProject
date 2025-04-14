909158c62a17b92bb068ee256d4a93ab0a73de05
Reyhan <reyhan.salimova.rs@gmail.com>
2025-04-15 01:32:30 +0400

    DP-24:Create new test4.js file with functions doubleNumbers,toUpperCaseArray, extractNames, booleanToText

--- test4.js
+++ test4.js
@@ -1,25 +1,36 @@
 function doubleNumbers(arr) {
     let doubles = arr.map((num) => num * 2);
+
     return doubles;
 }
 
-
 function toUpperCaseArray(arr) {
     arr = arr.map((num) => num.toUpperCase());
-    return arr;
 
+    return arr;
 }
 
 function addIndex(arr){
-    let arr1 = arr.map((num) => num + num)
+    let arr1 = arr.map((num) => num + num);
+
     return arr1;
 }
 
 function extractNames(people) {
     let arr = people.map(person => person.name);
-    return arr
+
+    return arr;
 }
 
+function booleanToText(arr) {
+    return arr.map(function(value) {
+        if (value === true) {
+            return "Yes";
+        } else {
+            return "No";
+        }
+    });
+}
 
 //test
 console.log(doubleNumbers([1,2,3]));
@@ -36,3 +47,7 @@ console.log(addIndex([1,2,3]));
 
 const names = extractNames(people);
 console.log(names);
+
+const bool = [true, false];
+const result1 = booleanToText(bool);
+console.log(result1);
