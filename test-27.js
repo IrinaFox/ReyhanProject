418f64d8054a51fba11d6d65822bfba19e37845b
Reyhan <reyhan.salimova.rs@gmail.com>
2025-04-22 16:03:54 +0400

    DP-27: Created functions: sortNumbersAsc, sortNumbersDesc, sortStringsAlphabetically, sortByLength

--- test-27.js
+++ test-27.js
@@ -1,12 +1,29 @@
 function sortNumbersAsc(arr) {
-    return arr.slice().sort((a, b) => a - b);
+    return arr.sort((a, b) => a - b);
 }
 
+function sortNumbersDesc(arr) {
+    return arr.sort((a, b) => b - a);
+}
 
+function  sortStringsAlphabetically(arr) {
+    return arr.sort()
+}
 
+function sortByLength(arr) {
+    return arr.sort((a, b) => a.length - b.length);
+}
 // test
 
 const numbers = [3, 1, 6, 8, 9, 10];
-const sorted = sortNumbersAsc(numbers);
+const fruits = ["banana", 'apple', 'lemon'];
 
-console.log(sorted);
+const sortedAsc = sortNumbersAsc(numbers);
+const sortedDesc = sortNumbersDesc(numbers);
+const sortedStrings = sortStringsAlphabetically(fruits);
+const sortLength = sortByLength(fruits);
+
+console.log(sortedAsc);
+console.log(sortedDesc);
+console.log(sortedStrings);
+console.log(sortLength);
