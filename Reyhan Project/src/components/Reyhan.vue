0815a75d22c97dda239ac2180fa14727a1be5e1d
Reyhan <reyhan.salimova.rs@gmail.com>
2025-10-06 11:18:01 +0400

    DP-42:I added changes to the Reyhan.vue file.

--- Reyhan Project/src/components/Reyhan.vue
+++ Reyhan Project/src/components/Reyhan.vue
@@ -3,9 +3,29 @@
 </script>
 
 <template>
-<p>I love Ira!!!</p>
+  <!-- Здесь пишется весь HTML код -->
+  <div class="container">
+    <h1>Здравствуй Дедушка Мороз</h1>
+    <p>Здравствуй Дедушка Мороз,борода из ваты.
+    Ты подарки нам принес?</p>
+    <input type="radio" name="answer" value="yes"> Да
+    <input type="radio" name="answer" value="no"> Нет
+    <input type="button" class="blue-btn" onclick="alert('Ваш ответ принят!')" value="Отправить">
+  </div>
 </template>
 
 <style scoped>
+.blue-btn {
+  background-color: #007bff; /* Синий цвет */
+  color: white;
+  border: none;
+  padding: 8px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
 
+/* При наведении */
+.blue-btn:hover {
+  background-color: #0056b3; /* Темнее при наведении */
+}
 </style>
