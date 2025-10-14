4ee518ff89851991f3e8e2a7760cce9a7d99f01a
Reyhan <reyhan.salimova.rs@gmail.com>
2025-10-14 10:17:18 +0400

    DP-43:Added components <div>, <p>, <span>, <b> to Reyhan.vue.

--- Reyhan Project/src/components/Header.vue
+++ Reyhan Project/src/components/Header.vue
@@ -8,6 +8,10 @@
 
 <style scoped>
 button {
-  background-color: aquamarine;
+  width: 300px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #47cc00;
+  border-radius: 10px;
 }
 </style>
--- Reyhan Project/src/components/Reyhan.vue
+++ Reyhan Project/src/components/Reyhan.vue
@@ -12,6 +12,26 @@
     <input type="radio" name="answer" value="no"> Нет
     <input type="button" class="blue-btn" onclick="alert('Ваш ответ принят!')" value="Отправить">
   </div>
+
+  <!-- Div с классом -->
+  <div class="container">
+    <h1>Приветствуем!</h1>
+    <p>Добро пожаловать на наш сайт</p>
+  </div>
+
+  <!-- Div с ID -->
+  <div id="header">
+    <nav>Заполните форму</nav>
+  </div>
+
+  <!-- Вложенные divы -->
+  <div class="wrapper">
+    <div class="header">
+      <div class="logo">IrRey</div>
+    </div>
+  </div>
+  <p>Тут могла бы быть <span style="color: red;">ваша</span> реклама.</p>
+  <p><b>Внимание:</b> Это лучшее что с вами случалось!</p>
 </template>
 
 <style scoped>
@@ -27,5 +47,15 @@
 /* При наведении */
 .blue-btn:hover {
   background-color: #0056b3; /* Темнее при наведении */
+}
+.container {
+  width: 300px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #988a8a;
+  border-radius: 10px;
+}
+b {
+  font-weight: bold;  /* или 700 */
 }
 </style>
