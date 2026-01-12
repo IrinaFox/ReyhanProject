e73ced9f48a2575dfcbb1faf4d23af9be891813b
Reyhan <reyhan.salimova.rs@gmail.com>
2026-01-12 16:13:08 +0400

    DP-48:I created a new button and added styles to it.

---
+++ Reyhan Project/src/components/buttons/NewButton.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  <button class="btn">Нажми меня</button>
+</template>
+
+<style scoped>
+.btn {
+  /* Content */
+  width: 200px;
+  height: 50px;
+
+  /* Padding */
+  padding: 10px 20px;
+
+  /* Border */
+  border: 2px solid #4a90e2;
+  border-radius: 8px;
+
+  /* Margin */
+  margin: 20px;
+
+  /* Box model control */
+  box-sizing: border-box;
+
+  /* Дополнительно для красоты */
+  background-color: #6aa9ff;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
