71c4d42ffa3f5356875963c9549406d1f8e91834
Reyhan <reyhan.salimova.rs@gmail.com>
2026-02-02 10:24:05 +0400

    DP-49:"Css - Вес селекторов".Added styles for tag, class, id,important.

--- Reyhan Project/src/components/buttons/NewButton.vue
+++ Reyhan Project/src/components/buttons/NewButton.vue
@@ -9,37 +9,40 @@
 </template>
 
 <style scoped>
+button {
+  background-color: purple;
+  color: yellow;
+}
+
 .btn {
-  /* Content */
   width: 200px;
   height: 50px;
-
-  /* Padding */
   padding: 10px 20px;
-
-  /* Border */
   border: 2px solid #4a90e2;
   border-radius: 8px;
-
-  /* Margin */
   margin: 20px;
-
-  /* Box model control */
   box-sizing: border-box;
-
-  /* Дополнительно для красоты */
   background-color: #6aa9ff;
   color: white;
   font-size: 16px;
   cursor: pointer;
 }
+
+#mainBtn {
+  background-color: orange;
+}
+
+button {
+  background-color: black !important;
+}
+
 .red-box {
   background-color: red;
   width: 200px;
   height: 100px;
   padding: 20px;
   border: 5px solid black;
-  box-sizing: border-box; /* учитывает padding и border в ширине и высоте */
+  box-sizing: border-box;
   color: white;
   margin-bottom: 20px;
 }
