597f39eb6d842c70e359259ec7a003dc83fa8381
Reyhan <reyhan.salimova.rs@gmail.com>
2026-02-16 11:10:43 +0400

    DP-50 "Css - Flex":Created a new component MyTable.vue and constrained its elements differently using different flex properties.

--- Reyhan Project/src/App.vue
+++ Reyhan Project/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Calculator from "@/components/Calculator/Calculator.vue";
 import NewButton from "@/components/buttons/NewButton.vue";
+import MyTable from "./components/MyTable.vue"
 </script>
 
 <template>
   <Calculator />
   <NewButton />
+  <MyTable />
 </template>
 
 <style scoped>
